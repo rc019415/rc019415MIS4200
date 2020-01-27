463279115566c44fc825886516e743532e9c34b4
Rachel Caviolo <rc019415@ohio.edu>
2020-01-27 11:39:48 -0500

    beginning of model

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace rc019415MIS4200.Models
+{
+    public class Student
+    {
+        public int studentID { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+
+    }
+}
